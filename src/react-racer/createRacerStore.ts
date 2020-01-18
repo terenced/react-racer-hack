17a01a7d6f954ee0628197c6c69350aea8f7edf3
terry <terry.dellino@gmail.com>
2020-01-18 13:26:08 -0500

    Switch to using racer-highway

--- src/react-racer/createRacerStore.ts
+++ src/react-racer/createRacerStore.ts
@@ -1,8 +1,41 @@
-import * as Racer from "racer";
+import * as racer from "racer";
+import * as Socket from "racer-highway/lib/browser/socket";
+
+const DEFAULT_CLIENT_OPTIONS = {
+  base: "/channel",
+  reconnect: true,
+  browserChannelOnly: false,
+  srvProtocol: undefined,
+  srvHost: undefined,
+  srvPort: undefined,
+  srvSecurePort: undefined,
+  timeout: 10000,
+  timeoutIncrement: 10000
+};
+
+// Since racer-highway is not bundled into the bundle from the server, we have to inject it here.
+// Stole the idea from here
+// https://github.com/dmapper/startupjs/blob/master/packages/model/src/getModel.js#L17
+racer.Model.prototype._createSocket = function() {
+  let clientOptions =
+    // @ts-ignore
+    (typeof window !== "undefined" && window.__racerHighwayClientOptions) ||
+    DEFAULT_CLIENT_OPTIONS;
+  return new Socket(clientOptions);
+};
+
+function createModel(bundle) {
+  const data = JSON.parse(bundle);
+  if (racer.util.isServer) {
+    const model = new racer.Model();
+    model.unbundle(data);
+    return model;
+  }
+  return racer.createModel(data);
+}
 
 export default function createRacerStore(bundle: string) {
-  const $model = new Racer.Model();
-  $model.unbundle(JSON.parse(bundle));
-
+  const $model = createModel(bundle);
+  $model.root.unloadDelay = 3000;
   return { $model };
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,31 @@
 import * as express from "express";
-import * as racerBrowserChannel from "racer-browserchannel";
-import render from "./render";
+import * as racer from "racer";
 import * as shareDbMongo from "sharedb-mongo";
-
-const racer = require("racer");
+import * as highway from "racer-highway";
+import render from "./render";
 
 racer.use(require("racer-bundle"));
 const backend = racer.createBackend({
-  db: shareDbMongo("mongodb://localhost:27017/racer-todos")
+  db: shareDbMongo("mongodb://localhost:27017/react-racer-hack")
 });
 
+const handlers = highway(backend);
+
 const app = express();
-app.use(racerBrowserChannel(backend));
-app.use(backend.modelMiddleware());
 app.use("/dist", express.static("dist"));
 app.use("/img", express.static("img"));
-
-app.get("*", async (_req, res) => {
-  const model = backend.createModel({fetchOnly: true});
+app.use(handlers.middleware);
+app.use(backend.modelMiddleware());
+ 
+app.get("*", async (req, res) => {
+  // @ts-ignore
+  const model = req?.model!;
   model.bundle((err, bundle) => {
     if (err) {
       res.send(err);
       return;
     }
-    
+
     const content = render(JSON.stringify(bundle));
     res.send(content);
   });
